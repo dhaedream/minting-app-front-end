ce8e2734f4b4db37f549b1bcf2de73054c1dedff
Dhaesha M <dhaesha1@gmail.com>
2022-04-13 18:56:32 -0400

    adding more to set up routes + links

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,7 +1,8 @@
-import React from "react";
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
 
 const Navbar = () => {
-  return <div>Navbar</div>;
+  return <div></div>;
 };
 
 export default Navbar;
