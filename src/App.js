b956245d73ed0eb4ef53cee485eb9acfe0c9aee7
Dhaesha M <dhaesha1@gmail.com>
2022-04-13 22:30:09 -0400

    setting up homepage to be routed into App.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
+import { Routes, Route } from "react-router-dom";
 import "./App.css";
+import Home from "./pages/HomePage";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header"></header>
-    </div>
+    <>
+      <Routes>
+        <Route path="/" element={Home} />
+      </Routes>
+    </>
   );
 }
 
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,4 +1,7 @@
 import React from "react";
+import Navbar from "../components/navbar/Navbar";
+import Discover from "../components/discover/Discover";
+import Footer from "../components/footer/Footer";
 
 const Home = () => {
   return <div>Home</div>;
