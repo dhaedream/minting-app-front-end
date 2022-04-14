93141ead88c4990157e806bf671210058f731d8c
Dhaesha M <dhaesha1@gmail.com>
2022-04-13 23:47:05 -0400

    routes working

--- src/App.js
+++ src/App.js
@@ -4,17 +4,14 @@ import "./App.css";
 import HomePage from "./pages/HomePage";
 import MintPage from "./pages/MintPage";
 import CommunityPage from "./pages/CommunityPage";
-import Navbar from "./components/navbar/Navbar";
 
 function App() {
   return (
-    <div>
-      <Routes>
-        <Route path="/" element={HomePage} />
-        <Route path="/mint" element={MintPage} />
-        <Route path="/community" element={CommunityPage} />
-      </Routes>
-    </div>
+    <Routes>
+      <Route path="/" element={<HomePage />} />
+      <Route path="/mint" element={<MintPage />} />
+      <Route path="/community" element={<CommunityPage />} />
+    </Routes>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 import React from "react";
-import ReactDOM from "react-dom";
+import { render } from "react-dom";
+import { BrowserRouter } from "react-router-dom";
 import App from "./App";
 
-import { BrowserRouter } from "react-router-dom";
-
-ReactDOM.render(
+const rootElement = document.getElementById("root");
+render(
   <BrowserRouter>
     <App />
   </BrowserRouter>,
-  document.getElementById("root")
+  rootElement
 );
