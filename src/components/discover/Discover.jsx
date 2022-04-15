9f94e02ad4a0d3f341ad74110c5616891c1575de
Dhaesha M <dhaesha1@gmail.com>
2022-04-14 23:49:13 -0400

    discover class names added to style

--- src/components/discover/Discover.jsx
+++ src/components/discover/Discover.jsx
@@ -3,20 +3,20 @@ import alienPet from "../../assets/alien-pet-art.png";
 
 const Discover = () => {
   return (
-    <div>
-      <div className="left">
+    <div className="discover">
+      <div className="discover-left">
         <h1>The Coolest NFT sidekicks are here! Rockstar NFT Pets!</h1>
         <p>
           I believe that space travel will one day become as common as airline
           travel is today. I’m convinced, however, that the true future of space
           travel does not lie with government agencies.
         </p>
-        <button>Explore</button>
+        <button className="discover-button">Explore</button>
       </div>
-      <div className="right">
-        <img src={alienPet} alt="alien-pet" />
-        <card>
-          <span>Meta in Style</span>
+      <div className="discover-right">
+        <img src={alienPet} alt="alien-pet" className="discover-art" />
+        <card className="discover-accent-card">
+          <span className="discover-accent-span">Meta in Style</span>
           <p>Meta Sidekicks</p>
         </card>
       </div>
