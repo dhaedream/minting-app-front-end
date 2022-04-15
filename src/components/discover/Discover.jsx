b896a56864581885642731342565c14b5d693795
Dhaesha M <dhaesha1@gmail.com>
2022-04-14 23:40:55 -0400

    discover jsx

--- src/components/discover/Discover.jsx
+++ src/components/discover/Discover.jsx
@@ -4,9 +4,13 @@ const Discover = () => {
   return (
     <div>
       <div className="left">
-        <h1></h1>
-        <p></p>
-        <button></button>
+        <h1>The Coolest NFT sidekicks are here! Rockstar NFT Pets!</h1>
+        <p>
+          I believe that space travel will one day become as common as airline
+          travel is today. I’m convinced, however, that the true future of space
+          travel does not lie with government agencies.
+        </p>
+        <button>Explore</button>
       </div>
       <div className="right">
         <img />
