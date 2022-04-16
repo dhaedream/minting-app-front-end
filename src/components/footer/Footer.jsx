0a01fa036fa56ab8a10737a91ca837a58e02155b
Dhaesha M <dhaesha1@gmail.com>
2022-04-16 14:47:15 -0400

    footer jsx

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,7 +1,17 @@
 import React from "react";
+import "./footer.css";
 
 const Footer = () => {
-  return <div>Footer</div>;
+  return (
+    <div>
+      <div className="footer-links-container">
+        <p>Home</p>
+        <p>Mint</p>
+        <p>Community</p>
+        <button className="navbar__button">Connect</button>
+      </div>
+    </div>
+  );
 };
 
 export default Footer;
