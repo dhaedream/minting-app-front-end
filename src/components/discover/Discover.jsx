ebacb747423eb963720dc76fa11218f9597218aa
Dhaesha M <dhaesha1@gmail.com>
2022-06-24 20:25:10 -0400

    making app more responsive

--- src/components/discover/Discover.jsx
+++ src/components/discover/Discover.jsx
@@ -6,13 +6,19 @@ const Discover = () => {
   return (
     <div className="discover">
       <div className="discover-left">
-        <h1>The Coolest NFT sidekicks are here! Rockstar NFT Pets!</h1>
-        <p>
-          I believe that space travel will one day become as common as airline
-          travel is today. I’m convinced, however, that the true future of space
-          travel does not lie with government agencies.
-        </p>
-        <button className="discover-button">Explore</button>
+        <div className="discover-left-top">
+          <h1 className="discover-header">
+            The Coolest NFT sidekicks are here!
+          </h1>
+        </div>
+        <div className="discover-left-bottom">
+          <p>
+            I believe that space travel will one day become as common as airline
+            travel is today. I’m convinced, however, that the true future of
+            space travel does not lie with government agencies.
+          </p>
+          <button className="discover-button">Explore</button>
+        </div>
       </div>
       <div className="discover-right">
         <img src={alienPet} alt="alien-pet" className="discover-art" />
